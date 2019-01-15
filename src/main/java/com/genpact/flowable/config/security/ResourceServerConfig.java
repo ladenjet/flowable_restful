6cf4d3ca91d1fb8cf2f5ade78ca8c2e35f88fbdd
Xia shengchun <shengchun.xia@genpact.com>
2019-01-15 16:41:39 +0800

    删除没用的配置

--- src/main/java/com/genpact/flowable/config/security/ResourceServerConfig.java
+++ src/main/java/com/genpact/flowable/config/security/ResourceServerConfig.java
@@ -1,13 +1,9 @@
 package com.genpact.flowable.config.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
-
-import com.genpact.flowable.config.security.interceptor.MyFilterSecurityInterceptor;
 
 /**
  * 配置类
@@ -17,14 +13,11 @@ import com.genpact.flowable.config.security.interceptor.MyFilterSecurityIntercep
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	@Autowired
-	private MyFilterSecurityInterceptor myFilterSecurityInterceptor;
+
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
     	http.csrf().disable();
-//    	动态权限
-    	http.addFilterBefore(myFilterSecurityInterceptor, FilterSecurityInterceptor.class);
 
         http.authorizeRequests()
         //authorize to swagger
